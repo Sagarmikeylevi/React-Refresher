9e5ef9de3ea1d9f2d5846b3ec46bb02132112c95
unknown <sagarmikeylevi@gmail.com>
2023-09-04 21:20:16 +0530

    outputing list data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import MainHeader from "./components/MainHeader";
+import PostsList from "./components/PostsList";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  const showModalHandler = () => {
+    setModalIsVisible(true);
+  };
+
+  const hideModalHandler = () => {
+    setModalIsVisible(false);
+  };
+
+  return (
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostsList
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
+    </>
+  );
 }
 
 export default App;
